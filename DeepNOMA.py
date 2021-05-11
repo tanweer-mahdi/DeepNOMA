1d3fdad6cfa0fbbc8805b5abf7947827c51ac5b2
tanweer-mahdi <shahmahdi.hasan@uon.edu.au>
2021-05-11 15:05:41 +1000

    Tried training DNNs in a for loop for different training sets. However it showed the same behaviours as it showed with KFold. Still trying to find a way to fix it.

--- DeepNOMA.py
+++ DeepNOMA.py
@@ -1,3 +1,5 @@
+import pdb
+
 import torch.utils.data
 from torch import nn
 from torch import optim
@@ -7,16 +9,16 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from sklearn.model_selection import KFold
 # loading data
-training = 'training_set.npy'
-labels = 'labels.npy'
+training = 'training_set_0.npy'
+labels = 'labels_0.npy'
 
 # exploring data
 train_exp = np.load(training, mmap_mode= 'r')
 print("Number of Features:", train_exp.shape[0])
 print("Number of Training Samples:", train_exp.shape[1])
 
-# creating dataset
-class nomadata(Dataset):
+# creating dataset class
+class NOMAdata(Dataset):
     def __init__(self, training, labels):
         self.mmapped = np.load(training)
         #self.mmapped = np.load(training, mmap_mode = 'r')
@@ -30,21 +32,18 @@ class nomadata(Dataset):
         return sample
 
 
-nomadata = nomadata(training,labels)
-
-
 # define the network structure in a dictionary
 structure = {
-    'input_layer' : [280 , 250],
-    'hidden_1' : [250, 250],
-    'hidden_2' : [250, 250],
-    'hidden_3' : [250, 250],
-    'hidden_4' : [250, 250],
-    'hidden_5' : [250, 250],
-    'output_layer': [250, 60],
+    'input_layer' : [280 , 260],
+    'hidden_1' : [260, 260],
+    'hidden_2' : [260, 260],
+    'hidden_3' : [260, 260],
+    'hidden_4' : [260, 260],
+    'hidden_5' : [260, 260],
+    'output_layer': [260, 60],
 }
 
-
+# creating the nn.Module class
 class DeepNOMA(nn.Module):
     def __init__(self, structure):
         super(DeepNOMA, self).__init__()
@@ -88,75 +87,83 @@ class DeepNOMA(nn.Module):
 
 
 
-# Codes for visualizing logs
+# Codes for visualizing the model
 writer = SummaryWriter('DeepNOMA')
 model = DeepNOMA(structure)
+nomadata = NOMAdata(training, labels)
 nomadata_loader = DataLoader(nomadata, batch_size= 20)
 writer.add_graph(model, next(iter(nomadata_loader))[0])
 writer.close()
 
 
-# Training the DNN
+# Training the DNN for multiple ensembles
+ensemble = 5
 lossfun = nn.BCEWithLogitsLoss(reduction='mean')
-optimizer = optim.Adam(model.parameters(), lr = 0.0005, amsgrad=False, weight_decay= 0)
+optimizer = optim.Adam(model.parameters(), lr=0.0005, amsgrad=True, weight_decay=0)
 cv = KFold(n_splits=5)
 
-for fold, (train_idx, test_idx) in enumerate(cv.split(nomadata)):
-
-    # creating the sampler
-    train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
-    test_sampler = torch.utils.data.SubsetRandomSampler(test_idx)
-
-    trainloader = DataLoader(nomadata, batch_size = 250, sampler  = train_sampler)
-    testloader = DataLoader(nomadata, batch_size= 250, sampler = test_sampler)
-
-    # resetting the parameters
-    # model = DeepNOMA(structure)
+for e in range(ensemble):
+    training = 'training_set_' + str(e) + '.npy'
+    labels = 'labels_' + str(e) + '.npy'
+    nomadata = NOMAdata(training, labels)
     for layer in model.children():
         if hasattr(layer, 'reset_parameters'):
             layer.reset_parameters()
+    # model = DeepNOMA(structure)
 
 
-    running_loss = 0
-    if fold == 0:
-        for epoch in range(200):
-            for i, val in enumerate(trainloader):
-                inputs, targets = val
-                # clear the gradients
-                optimizer.zero_grad()
-                # model output
-                yhat = model(inputs)
-                # calculate loss
-                loss = lossfun(yhat, targets)
-                # backprop
-                loss.backward()
-                # update model parameter
-                optimizer.step()
-                # loggin training performance
-                running_loss += loss.item()
+    for fold, (train_idx, test_idx) in enumerate(cv.split(nomadata)):
 
-                if i % 20 == 19:
-                    # # calculating validation loss
-                    model.eval()
-                    validation_loss = 0
-                    for j, batch in enumerate(testloader):
-                        test, labels = batch
-                        ypred = model(test)
-                        runval = lossfun(ypred, labels)
-                        validation_loss += runval.item()
+        # creating the sampler
+        train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
+        test_sampler = torch.utils.data.SubsetRandomSampler(test_idx)
 
-                    writer.add_scalars('Training/Validation Loss', {'Training loss': running_loss/20, 'Validation Loss': validation_loss/j}, epoch*len(trainloader) + i)
+        trainloader = DataLoader(nomadata, batch_size = 250, sampler  = train_sampler)
+        testloader = DataLoader(nomadata, batch_size= 250, sampler = test_sampler)
 
-                    model.train()
-                    print(running_loss/20, validation_loss/j, fold)
-                    running_loss = 0
+        # resetting the parameters
+        # model = DeepNOMA(structure)
+
+        running_loss = 0
+        if fold == 0:
+            for epoch in range(5):
+                for i, val in enumerate(trainloader):
+                    inputs, targets = val
+                    # clear the gradients
+                    optimizer.zero_grad()
+                    # model output
+                    yhat = model(inputs)
+                    # calculate loss
+                    loss = lossfun(yhat, targets)
+                    # backprop
+                    loss.backward()
+                    # update model parameter
+                    optimizer.step()
+                    # loggin training performance
+                    running_loss += loss.item()
+
+                    if i % 20 == 19:
+                        # # calculating validation loss
+                        model.eval()
+                        validation_loss = 0
+                        for j, batch in enumerate(testloader):
+                            test, labels = batch
+                            ypred = model(test)
+                            runval = lossfun(ypred, labels)
+                            validation_loss += runval.item()
+
+                        writer.add_scalars('Training/Validation Loss', {'Training loss': running_loss/20, 'Validation Loss': validation_loss/j}, epoch*len(trainloader) + i)
+
+                        model.train()
+                        print(running_loss/20, validation_loss/j, fold, e)
+                        running_loss = 0
 
 
 
-    # evaluating model for current fold
 
 
 
-# Saving the entire model
-save_path = 'model.pth'
-torch.save(model.state_dict(), save_path)
+
+    # Saving the entire model
+    save_path = 'model' + str(e) + '.pth'
+    torch.save(model.state_dict(), save_path)
