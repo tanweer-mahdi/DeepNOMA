45028ad4b0e416c5bea931cfa53416a5744403f4
tanweer-mahdi <shahmahdi.hasan@uon.edu.au>
2021-05-31 10:44:57 +1000

    This version of ULGFNOMA is important 1. I start to generate for a moderate size MTC 2. I have adopted a new convention of naming training data, labels and dev data. 3. I have used pa = 0.1

--- ULGFNOMAdata.py
+++ ULGFNOMAdata.py
@@ -2,31 +2,31 @@ import numpy as np
 import pdb
 from sklearn.model_selection import train_test_split
 import pdb
+
 # number of models in the ensemble
 ensemble = 5
-P = 125000  # number of samples
-dev_sample = 40000 # number of dev samples
+P = 20  # number of samples
+dev_sample = 20  # number of dev samples
 # create a dummy system model
-N = 60  # number of UE
-M = 40  # number of subcarrier
-distances = np.random.rand(N)*0.2 # cell radius is 200 meter
-noisevar = 1e-11 # noise variance
-J = 9 # number of slots
-pa = 0.05 # activation ratio
-tp = 20 # transmit power in dBm
-codebook = 'sparse' # choice of codebook
+N = 96  # number of UE
+M = 64  # number of subcarrier
+distances = np.random.rand(N) * 0.2  # cell radius is 200 meter
+noisevar = 1e-11  # noise variance
+J = 9  # number of slots
+pa = 0.1  # activation ratio
+tp = 20  # transmit power in dBm
+codebook = 'sparse'  # choice of codebook
 
 if codebook == 'sparse':
-    phi = np.zeros((2*M, N))
-    nz_entries = int(M/2)  # number of non-zero entries
+    phi = np.zeros((2 * M, N))
+    nz_entries = int(M / 2)  # number of non-zero entries
     for i in range(phi.shape[1]):
-        nz_index = np.random.choice(np.arange(M), size=nz_entries, replace=False)
+        nz_index = np.random.choice(np.arange(2 * M), size=nz_entries, replace=False)
         phi[nz_index, i] = np.random.normal(0, 1, (nz_entries,))
 elif codebook == 'dense':
-    phi = np.random.normal(0, 1, (2*M, N))
+    phi = np.random.normal(0, 1, (2 * M, N))
 else:
     pass
-
 
 # normalizing codebook
 vecnorm = 1 / np.linalg.norm(phi, 2, axis=0)
@@ -35,11 +35,11 @@ phi = np.dot(phi, np.diag(vecnorm))  # normalizing each sequence to unit norm ve
 
 # Generate training samples
 def gensample(N, M, J, phi, noisevar):
-    M = 2*M
+    M = 2 * M
     au = int(np.ceil(pa * N))
     # au = np.random.randint(1, int(np.ceil(pa * N)) + 1)
     uset = np.random.choice(np.arange(N), size=au, replace=False)
-    labels = np.zeros(N,dtype='float32')
+    labels = np.zeros(N, dtype='float32')
     labels[uset] = 1
     # creating the combined dictionary
     C = np.diag(phi[:, uset[0]])
@@ -53,19 +53,17 @@ def gensample(N, M, J, phi, noisevar):
     #     cv = np.hstack((cv,t))
 
     # creating channel vectors
-    cv = np.zeros((M*au, J))
+    cv = np.zeros((M * au, J))
 
     for slot in range(J):
         uchannel = np.zeros((M, au))
 
         for i, val in enumerate(uset):
-            rp = -128.1 - 36.7*np.log10(distances[val])+tp;
-            rp = np.power(10, rp/10)
-            uchannel[:, i] = np.random.normal(0, 1, (M, ))*np.sqrt(rp)
+            rp = -128.1 - 36.7 * np.log10(distances[val]) + tp;
+            rp = np.power(10, rp / 10)
+            uchannel[:, i] = np.random.normal(0, 1, (M,)) * np.sqrt(rp)
 
         cv[:, slot] = np.ravel(uchannel)
-
-
 
     y = np.dot(C, cv)
     y += np.random.normal(0, noisevar, (M, J))
@@ -75,49 +73,46 @@ def gensample(N, M, J, phi, noisevar):
     mu = np.mean(y)
     sigma = np.std(y)
 
-    y = (y-mu)/sigma
+    y = (y - mu) / sigma
 
     return y.astype(dtype='float32'), labels
-    #return np.ravel(y).astype(dtype='float32'), labels
-
-
+    # return np.ravel(y).astype(dtype='float32'), labels
 
 
 for ii in range(ensemble):
-    dataset = np.zeros((2* M * J, P), dtype='float32')
+    dataset = np.zeros((2 * M * J, P), dtype='float32')
     labels = np.zeros((N, P), dtype='float32')
 
     for j in range(P):
-        data, label = gensample(N,M,J,phi,noisevar)
+        data, label = gensample(N, M, J, phi, noisevar)
         dataset[:, j] = data
         labels[:, j] = label
 
-
-    training_name = 'real_training_set_' + str(ii) + '.npy'
-    labels_name = 'real_labels_' + str(ii) + '.npy'
+    training_name = 'training_set_' + str(M) + '_' + str(ii) + '.npy'
+    labels_name = 'labels_' + str(M) + '_' + str(ii) + '.npy'
+    print(training_name)
+    print(labels_name)
     # ## saving training data
-    with open(training_name,'wb') as file: # using "with" while opening file is a good idea. It properly closes the file.
+    with open(training_name, 'wb') as file:  # using "with" while opening file is a good idea. It properly closes the file.
         np.save(file, dataset)
 
     ## saving training labels
-    with open(labels_name,'wb') as file:
+    with open(labels_name, 'wb') as file:
         np.save(file, labels)
-
-
 
 # creating dev data
 
-dataset = np.zeros((2* M * J, dev_sample), dtype='float32')
+dataset = np.zeros((2 * M * J, dev_sample), dtype='float32')
 labels = np.zeros((N, dev_sample), dtype='float32')
 
 for i in range(dev_sample):
-    data, label = gensample(N,M,J,phi,noisevar)
+    data, label = gensample(N, M, J, phi, noisevar)
     dataset[:, i] = data
     labels[:, i] = label
 
 # saving dev data
-with open('real_dev_data.npy', 'wb') as file:
+with open('dev_data_' + str(M) + '.npy', 'wb') as file:
     np.save(file, dataset)
 
-with open('real_dev_labels.npy', 'wb') as file:
+with open('dev_labels_' + str(M) + '.npy', 'wb') as file:
     np.save(file, labels)
